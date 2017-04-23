8bc5b2acf81b510d7618e8a818be771b9a24d6ca
sin_sin <sinsinpub@gmail.com>
2017-04-23 12:33:57 +0800

    Update travis for my repo
    * Add packaging release.zip only task to grunt
    * Deploy release.zip to github only on release branch
    * Cancel gitter notifications

--- Gruntfile.js
+++ Gruntfile.js
@@ -548,6 +548,10 @@ module.exports = function(grunt) {
 		'qunit'
 	]);
 	
+	grunt.registerTask('package', [
+		'compress:release'
+	]);
+	
 	grunt.registerTask('webstore', [
 		'compress:release',
 		'webstore_upload:kc3kai'
